8a6a1a1d4d31f8ff63f55fce7b230afcf7027297
George Whiteside <george@georgewhiteside.net>
2012-05-04 09:26:02 -0500

    added new class and raw data file

---
+++ src/net/georgewhiteside/android/abstractart/BattleEnemy.java
@@ -0,0 +1,13 @@
+package net.georgewhiteside.android.abstractart;
+
+import android.content.Context;
+
+public class BattleEnemy
+{
+	Context context;
+	
+	public BattleEnemy(Context context)
+	{
+		this.context = context;
+	}
+}
